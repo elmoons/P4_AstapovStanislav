cb84236fa64060ef3c2b62efa886848f1958c583
Stanislav Astapov <elmoons@yandex.ru>
2024-06-20 20:48:01 +0300

    20.06.24 commit(3)

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from datetime import datetime
 from enum import Enum
-
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
+
 app = FastAPI(
-    title="Сервис для шифрования"
+    title="Сервис для шифрования и дешифрования текста."
 )
 
+alphabet = ",.:(_)-0123456789АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+EXITING_LOGINS = ["bob1997", "carl-stalker1337", "ivan_v_tanke"]
 
-class EncryptType(Enum):
+
+class EncryptMethod(Enum):
     caesar: "caesar"
     vigenere: "vigenere"
 
@@ -21,10 +24,10 @@ class Users(BaseModel):
 
 
 class MethodsOfEncryption(BaseModel):
-    id: int
-    caption: str
+    method: EncryptMethod
+    caption: str = Field(max_length=30)
     json_params: dict
-    descriptions: str
+    descriptions: str = Field(max_length=1000)
 
 
 class Sessions(BaseModel):
@@ -45,14 +48,13 @@ fake_users = [
 ]
 
 
-@app.get("/caesar")
-def caesar_method(word_for_encrypt: str, number_of_shifts: int):
-    word_for_encrypt_upper = word_for_encrypt.upper()
-    alphabet = ",.:(_)-0123456789АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+@app.get("/encrypt/caesar")
+def encrypt_caesar_method(text_for_encrypt: str, number_of_shifts: int):
+    text_for_encrypt_upper = text_for_encrypt.upper()
     encrypted_text = []
     alphabet_size = len(alphabet)
 
-    for char in word_for_encrypt_upper:
+    for char in text_for_encrypt_upper:
         if char in alphabet:
             original_index = alphabet.index(char)
             new_index = (original_index + number_of_shifts) % alphabet_size
@@ -63,16 +65,32 @@ def caesar_method(word_for_encrypt: str, number_of_shifts: int):
     return ''.join(encrypted_text)
 
 
-@app.get("/vigenere")
-def vigenere_method(word_for_encrypt: str, keyword: str):
-    word_for_encrypt_upper = word_for_encrypt.upper()
+@app.get("/decrypt/caesar")
+def decrypt_caesar_method(text_for_decrypt: str, number_of_shifts: int):
+    text_for_decrypt_upper = text_for_decrypt.upper()
+    decrypted_text = []
+    alphabet_size = len(alphabet)
+
+    for char in text_for_decrypt_upper:
+        if char in alphabet:
+            encrypted_index = alphabet.index(char)
+            new_index = (encrypted_index - number_of_shifts) % alphabet_size
+            decrypted_text.append(alphabet[new_index])
+        else:
+            decrypted_text.append(char)
+
+    return ''.join(decrypted_text)
+
+
+@app.get("/encrypt/vigenere")
+def encrypt_vigenere_method(text_for_encrypt: str, keyword: str):
+    text_for_encrypt_upper = text_for_encrypt.upper()
     keyword_upper = keyword.upper()
-    alphabet = ",.:(_)-0123456789АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
     encrypted_text = []
     alphabet_size = len(alphabet)
     keyword_len = len(keyword_upper)
 
-    for i, char in enumerate(word_for_encrypt_upper):
+    for i, char in enumerate(text_for_encrypt_upper):
         if char in alphabet:
             original_index = alphabet.index(char)
             key_char = keyword_upper[i % keyword_len]
@@ -83,3 +101,25 @@ def vigenere_method(word_for_encrypt: str, keyword: str):
             encrypted_text.append(char)
 
     return ''.join(encrypted_text)
+
+
+@app.get("/decrypt/vigenere")
+def decrypt_vigenere_method(text_for_decrypt: str, keyword: str):
+    text_for_decrypt_upper = text_for_decrypt.upper()
+    keyword_upper = keyword.upper()
+    decrypted_text = []
+    alphabet_size = len(alphabet)
+    keyword_len = len(keyword_upper)
+
+    for i, char in enumerate(text_for_decrypt_upper):
+        if char in alphabet:
+            encrypted_index = alphabet.index(char)
+            key_char = keyword_upper[i % keyword_len]
+            key_index = alphabet.index(key_char)
+            new_index = (encrypted_index - key_index) % alphabet_size
+            decrypted_text.append(alphabet[new_index])
+        else:
+            decrypted_text.append(char)
+
+    return ''.join(decrypted_text)
+
